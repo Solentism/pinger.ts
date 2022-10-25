fcaee363a0f83e4c4b3d697d4c09a80344d3d502
Kieran Schuler <commit@k1e.io>
2022-10-25 11:24:01 +0100

    update paths

--- testfile.js
+++ testfile.js
@@ -2,10 +2,10 @@ const pinger = require('./dist/index.js');
 pinger.ping("discord.com", 2000, true);
 pinger.webserver(8080)
 
-// const {Group} = require('../index');
+// const {Group} = require('./dist/index.js');
 // let g = new Group();
 // ['wubzy.xyz', 'k1e.io'].forEach(u => g.add(u, 5000));
 // g.startAll();
 
-// const pinger = require('../index.js')
+// const pinger = require('./dist/index.js')
 // pinger.dping(webhooktoken='V8LP8mnbnCkJqdwWZrCs7ib9xVnihhiXheAWRPHnrkZt6', webhookid='109337362537264548', url='https://k1e.io', interval=10000, flipDM=true, noURLFix=false)
